fe785cde0af3432375651784880275459b4c05c4
Nebuleon Fumika <nebuleon@porygon>
2014-02-01 09:23:54 +0000

    tile.cpp: Rewrite clipped tile renderers to use loops instead of Duff's devices.
    This reduces the code size, helping arhitectures having a small instruction cache or a small loop prefetcher, and allows the compiler to perform loop optimisations as needed.
    
    Based on https://github.com/libretro/snes9x-next/commit/b5f851c273e7fea13faababd4790b115c54a1b37 by twinaphex; augmented to cover all Duff's devices.

--- pocketsnes/snes9x/tile.cpp
+++ pocketsnes/snes9x/tile.cpp
@@ -844,17 +844,11 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		for (l = LineCount; l > 0; l--, bp += 8 * PITCH, Offset += GFX.PPL) \
 		{ \
 			w = Width; \
-			switch (StartPixel) \
-			{ \
-				case 0: DRAW_PIXEL(0, Pix = bp[0]); if (!--w) break; \
-				case 1: DRAW_PIXEL(1, Pix = bp[1]); if (!--w) break; \
-				case 2: DRAW_PIXEL(2, Pix = bp[2]); if (!--w) break; \
-				case 3: DRAW_PIXEL(3, Pix = bp[3]); if (!--w) break; \
-				case 4: DRAW_PIXEL(4, Pix = bp[4]); if (!--w) break; \
-				case 5: DRAW_PIXEL(5, Pix = bp[5]); if (!--w) break; \
-				case 6: DRAW_PIXEL(6, Pix = bp[6]); if (!--w) break; \
-				case 7: DRAW_PIXEL(7, Pix = bp[7]); break; \
-			} \
+			uint8 n = StartPixel; \
+			do { \
+				DRAW_PIXEL(n, Pix = bp[n]); \
+				n++; \
+			} while (--w > 0); \
 		} \
 	} \
 	else \
@@ -864,17 +858,11 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		for (l = LineCount; l > 0; l--, bp += 8 * PITCH, Offset += GFX.PPL) \
 		{ \
 			w = Width; \
-			switch (StartPixel) \
-			{ \
-				case 0: DRAW_PIXEL(0, Pix = bp[7]); if (!--w) break; \
-				case 1: DRAW_PIXEL(1, Pix = bp[6]); if (!--w) break; \
-				case 2: DRAW_PIXEL(2, Pix = bp[5]); if (!--w) break; \
-				case 3: DRAW_PIXEL(3, Pix = bp[4]); if (!--w) break; \
-				case 4: DRAW_PIXEL(4, Pix = bp[3]); if (!--w) break; \
-				case 5: DRAW_PIXEL(5, Pix = bp[2]); if (!--w) break; \
-				case 6: DRAW_PIXEL(6, Pix = bp[1]); if (!--w) break; \
-				case 7: DRAW_PIXEL(7, Pix = bp[0]); break; \
-			} \
+			uint8 n = StartPixel; \
+			do { \
+				DRAW_PIXEL(n, Pix = bp[7 - n]); \
+				n++; \
+			} while (--w > 0); \
 		} \
 	} \
 	else \
@@ -884,17 +872,11 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		for (l = LineCount; l > 0; l--, bp -= 8 * PITCH, Offset += GFX.PPL) \
 		{ \
 			w = Width; \
-			switch (StartPixel) \
-			{ \
-				case 0: DRAW_PIXEL(0, Pix = bp[0]); if (!--w) break; \
-				case 1: DRAW_PIXEL(1, Pix = bp[1]); if (!--w) break; \
-				case 2: DRAW_PIXEL(2, Pix = bp[2]); if (!--w) break; \
-				case 3: DRAW_PIXEL(3, Pix = bp[3]); if (!--w) break; \
-				case 4: DRAW_PIXEL(4, Pix = bp[4]); if (!--w) break; \
-				case 5: DRAW_PIXEL(5, Pix = bp[5]); if (!--w) break; \
-				case 6: DRAW_PIXEL(6, Pix = bp[6]); if (!--w) break; \
-				case 7: DRAW_PIXEL(7, Pix = bp[7]); break; \
-			} \
+			uint8 n = StartPixel; \
+			do { \
+				DRAW_PIXEL(n, Pix = bp[n]); \
+				n++; \
+			} while (--w > 0); \
 		} \
 	} \
 	else \
@@ -903,17 +885,11 @@ void S9xSelectTileConverter (int depth, bool8 hires, bool8 sub, bool8 mosaic)
 		for (l = LineCount; l > 0; l--, bp -= 8 * PITCH, Offset += GFX.PPL) \
 		{ \
 			w = Width; \
-			switch (StartPixel) \
-			{ \
-				case 0: DRAW_PIXEL(0, Pix = bp[7]); if (!--w) break; \
-				case 1: DRAW_PIXEL(1, Pix = bp[6]); if (!--w) break; \
-				case 2: DRAW_PIXEL(2, Pix = bp[5]); if (!--w) break; \
-				case 3: DRAW_PIXEL(3, Pix = bp[4]); if (!--w) break; \
-				case 4: DRAW_PIXEL(4, Pix = bp[3]); if (!--w) break; \
-				case 5: DRAW_PIXEL(5, Pix = bp[2]); if (!--w) break; \
-				case 6: DRAW_PIXEL(6, Pix = bp[1]); if (!--w) break; \
-				case 7: DRAW_PIXEL(7, Pix = bp[0]); break; \
-			} \
+			uint8 n = StartPixel; \
+			do { \
+				DRAW_PIXEL(n, Pix = bp[7 - n]); \
+				n++; \
+			} while (--w > 0); \
 		} \
 	}
 
